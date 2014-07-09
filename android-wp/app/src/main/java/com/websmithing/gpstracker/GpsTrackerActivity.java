2effcaaae6c8ae26c0a0c10ca9b0203b92980d38
Nick Fox <nicolaspatrickfox@hotmail.com>
2014-07-08 21:09:36 -0700

    added android-WP

---
+++ android-wp/app/src/main/java/com/websmithing/gpstracker/GpsTrackerActivity.java
@@ -0,0 +1,298 @@
+package com.websmithing.gpstracker;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.util.UUID;
+
+public class GpsTrackerActivity extends ActionBarActivity {
+    private static final String TAG = "GpsTrackerActivity";
+
+    // use the websmithing defaultUploadWebsite for testing and then check your
+    // location with your browser here: https://www.websmithing.com/gpstracker/displaymap.php
+    private String defaultUploadWebsite;
+
+    private static EditText txtUserName;
+    private static EditText txtWebsite;
+    private static Button trackingButton;
+
+    private boolean currentlyTracking;
+    private RadioGroup intervalRadioGroup;
+    private int intervalInMinutes = 1;
+    private AlarmManager alarmManager;
+    private Intent gpsTrackerIntent;
+    private PendingIntent pendingIntent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpstracker);
+
+        defaultUploadWebsite = getString(R.string.default_upload_website);
+
+        txtWebsite = (EditText)findViewById(R.id.txtWebsite);
+        txtUserName = (EditText)findViewById(R.id.txtUserName);
+        intervalRadioGroup = (RadioGroup)findViewById(R.id.intervalRadioGroup);
+        trackingButton = (Button)findViewById(R.id.trackingButton);
+        txtUserName.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
+        currentlyTracking = sharedPreferences.getBoolean("currentlyTracking", false);
+
+        intervalRadioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                        saveInterval();
+                    }
+                });
+
+        trackingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                trackLocation(view);
+            }
+        });
+    }
+
+    private void saveInterval() {
+        if (currentlyTracking) {
+            Toast.makeText(getApplicationContext(), R.string.user_needs_to_restart_tracking, Toast.LENGTH_LONG).show();
+        }
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        switch (intervalRadioGroup.getCheckedRadioButtonId()) {
+            case R.id.i1:
+                editor.putInt("intervalInMinutes", 1);
+                break;
+            case R.id.i5:
+                editor.putInt("intervalInMinutes", 5);
+                break;
+            case R.id.i15:
+                editor.putInt("intervalInMinutes", 15);
+                break;
+            case R.id.i30:
+                editor.putInt("intervalInMinutes", 30);
+                break;
+            case R.id.i60:
+                editor.putInt("intervalInMinutes", 60);
+                break;
+        }
+
+        editor.commit();
+    }
+
+    private void startAlarmManager() {
+        Log.d(TAG, "startAlarmManager");
+
+        Context context = getBaseContext();
+        alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        gpsTrackerIntent = new Intent(context, GpsTrackerAlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(context, 0, gpsTrackerIntent, 0);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
+        intervalInMinutes = sharedPreferences.getInt("intervalInMinutes", 1);
+
+        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime(),
+                intervalInMinutes * 60000, // 60000 = 1 minute
+                pendingIntent);
+    }
+
+    private void cancelAlarmManager() {
+        Log.d(TAG, "cancelAlarmManager");
+
+        if (alarmManager != null) {
+            alarmManager.cancel(pendingIntent);
+            alarmManager = null;
+        }
+    }
+
+    // called when trackingButton is tapped
+    protected void trackLocation(View v) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        if (!saveUserSettings()) {
+            return;
+        }
+
+        if (!checkIfGooglePlayEnabled()) {
+            return;
+        }
+
+        if (currentlyTracking) {
+            Toast.makeText(getApplicationContext(), R.string.tracking_has_now_stopped, Toast.LENGTH_LONG).show();
+
+            cancelAlarmManager();
+
+            currentlyTracking = false;
+            editor.putBoolean("currentlyTracking", false);
+            editor.putString("sessionID", "");
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.tracking_has_now_started, Toast.LENGTH_LONG).show();
+
+            startAlarmManager();
+
+            currentlyTracking = true;
+            editor.putBoolean("currentlyTracking", true);
+            editor.putFloat("totalDistanceInMeters", 0f);
+            editor.putBoolean("firstTimeGettingPosition", true);
+            editor.putString("sessionID",  UUID.randomUUID().toString());
+        }
+
+        editor.commit();
+        setTrackingButtonState();
+    }
+
+    private boolean saveUserSettings() {
+        if (textFieldsAreEmptyOrHaveSpaces()) {
+            return false;
+        }
+
+        if (!currentlyTracking) {
+            checkIfWebsiteIsReachable();
+        }
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        switch (intervalRadioGroup.getCheckedRadioButtonId()) {
+            case R.id.i1:
+                editor.putInt("intervalInMinutes", 1);
+                break;
+            case R.id.i5:
+                editor.putInt("intervalInMinutes", 5);
+                break;
+            case R.id.i15:
+                editor.putInt("intervalInMinutes", 15);
+                break;
+            case R.id.i30:
+                editor.putInt("intervalInMinutes", 30);
+                break;
+            case R.id.i60:
+                editor.putInt("intervalInMinutes", 60);
+                break;
+        }
+
+        editor.putString("userName", txtUserName.getText().toString().trim());
+        editor.putString("defaultUploadWebsite", txtWebsite.getText().toString().trim());
+
+        editor.commit();
+
+        return true;
+    }
+
+    private boolean textFieldsAreEmptyOrHaveSpaces() {
+        String tempUserName = txtUserName.getText().toString().trim();
+        String tempWebsite = txtWebsite.getText().toString().trim();
+
+        if (tempWebsite.length() == 0 || hasSpaces(tempWebsite) || tempUserName.length() == 0 || hasSpaces(tempUserName)) {
+            Toast.makeText(this, R.string.textfields_empty_or_spaces, Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean hasSpaces(String str) {
+        return ((str.split(" ").length > 1) ? true : false);
+    }
+
+    private void displayUserSettings() {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
+        intervalInMinutes = sharedPreferences.getInt("intervalInMinutes", 1);
+
+        switch (intervalInMinutes) {
+            case 1:
+                intervalRadioGroup.check(R.id.i1);
+                break;
+            case 5:
+                intervalRadioGroup.check(R.id.i5);
+                break;
+            case 15:
+                intervalRadioGroup.check(R.id.i15);
+                break;
+            case 30:
+                intervalRadioGroup.check(R.id.i30);
+                break;
+            case 60:
+                intervalRadioGroup.check(R.id.i60);
+                break;
+        }
+
+        txtWebsite.setText(sharedPreferences.getString("defaultUploadWebsite", defaultUploadWebsite));
+        txtUserName.setText(sharedPreferences.getString("userName", ""));
+    }
+
+    private boolean checkIfGooglePlayEnabled() {
+        if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS) {
+            return true;
+        } else {
+            Log.e(TAG, "unable to connect to google play services.");
+            Toast.makeText(getApplicationContext(), R.string.google_play_services_unavailable, Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+    private void checkIfWebsiteIsReachable() {
+        LoopjHttpClient.get(defaultUploadWebsite, null, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, org.apache.http.Header[] headers, byte[] responseBody) {
+                Log.e(TAG, "checkIfWebsiteIsReachable onSuccess statusCode: " + statusCode);
+            }
+            @Override
+            public void onFailure(int statusCode, org.apache.http.Header[] headers, byte[] errorResponse, Throwable e) {
+                Toast.makeText(getApplicationContext(), R.string.reachability_error, Toast.LENGTH_LONG).show();
+                Log.e(TAG, "checkIfWebsiteIsReachable onFailure statusCode: " + statusCode);
+            }
+        });
+    }
+
+    private void setTrackingButtonState() {
+        if (currentlyTracking) {
+            trackingButton.setBackgroundResource(R.drawable.green_tracking_button);
+            trackingButton.setTextColor(Color.BLACK);
+            trackingButton.setText(R.string.tracking_is_on);
+        } else {
+            trackingButton.setBackgroundResource(R.drawable.red_tracking_button);
+            trackingButton.setTextColor(Color.WHITE);
+            trackingButton.setText(R.string.tracking_is_off);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();  // Always call the superclass method first
+
+        displayUserSettings();
+        setTrackingButtonState();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+}
